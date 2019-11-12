05e2d0f6438afe36d16569b7365171debf162953
David Wicks <david.wicks@mapbox.com>
2019-11-12 16:42:49 -0500

    correct return type for listTilesets

--- src/listTilesets.ts
+++ src/listTilesets.ts
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch';
 
-/// Enumerates the tileset sources associated with the giver user
-export async function listTilesets(user, accessToken) {
+/// Enumerates the tilesets associated with the giver user
+export async function listTilesets(user, accessToken): Promise<string[]> {
   const url =
       `https://api.mapbox.com/tilesets/v1/${user}?access_token=${accessToken}`;
   try {
     const response = await fetch(url);
     if (response.ok) {
       const json = await response.json();
-      return json.map(source => source.id).join('\n');
+      return json.map(source => source.id);
     } else {
       const text = await response.text();
-      return {error: text};
+      return [text];
     }
   } catch (err) {
-    return {error: err.message};
+    return [err.message];
   }
 }
